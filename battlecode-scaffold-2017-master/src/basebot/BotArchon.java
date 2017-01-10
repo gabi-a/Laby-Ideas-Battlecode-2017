121ab956fa494c2459287a0b021a6625e6b09941
gabi-a <abrahams.gabi@gmail.com>
2017-01-10 18:34:31 +1100

    Removed the broken bot and updated basebot so errors will be caught and printed.

--- battlecode-scaffold-2017-master/src/basebot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/basebot/BotArchon.java
@@ -3,7 +3,11 @@ package basebot;
 import battlecode.common.*;
 
 public class BotArchon {
-	public static void turn(RobotController rc) {
+	
+	public static void turn(RobotController rc) throws GameActionException {
+		
 		
 	}
+	
 }
+
--- battlecode-scaffold-2017-master/src/basebot/RobotPlayer.java
+++ battlecode-scaffold-2017-master/src/basebot/RobotPlayer.java
@@ -7,25 +7,31 @@ public strictfp class RobotPlayer {
 	
 	public static void run(RobotController rc) throws GameActionException {
 		RobotPlayer.rc = rc;
-		
-		switch (rc.getType()) {
-        case ARCHON:
-            BotArchon.turn(rc);
-            break;
-        case GARDENER:
-        	BotGardener.turn(rc);
-            break;
-        case SOLDIER:
-        	BotSoldier.turn(rc);
-            break;
-        case LUMBERJACK:
-            BotLumberjack.turn(rc);
-            break;
-        case TANK:
-        	BotTank.turn(rc);
-        	break;
-        case SCOUT:
-        	BotScout.turn(rc);
+		while(true) {
+			try {
+				switch (rc.getType()) {
+				case ARCHON:
+				    BotArchon.turn(rc);
+				    break;
+				case GARDENER:
+					BotGardener.turn(rc);
+				    break;
+				case SOLDIER:
+					BotSoldier.turn(rc);
+				    break;
+				case LUMBERJACK:
+				    BotLumberjack.turn(rc);
+				    break;
+				case TANK:
+					BotTank.turn(rc);
+					break;
+				case SCOUT:
+					BotScout.turn(rc);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Clock.yield();
 		}
 	}
 		
