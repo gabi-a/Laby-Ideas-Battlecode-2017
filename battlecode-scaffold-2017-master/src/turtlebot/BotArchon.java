682c37cc9b5eae46e83f06a8847ebb229c15dd74
gabi-a <abrahams.gabi@gmail.com>
2017-01-11 07:35:23 +1100

    Not much

--- battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
@@ -7,7 +7,14 @@ import java.util.logging.Logger;
 public class BotArchon {
 	static RobotController rc;
 	
-	public static void turn(RobotController rc) {
+	public static void turn(RobotController rc) throws GameActionException {
 		BotArchon.rc = rc;
+		
+		int gardenersCount = Comms.getNumGardeners(rc);
+		
+		if(gardenersCount < 2) {
+			
+		}
+		
 	}
 }
--- battlecode-scaffold-2017-master/src/turtlebot/Comms.java
+++ battlecode-scaffold-2017-master/src/turtlebot/Comms.java
@@ -3,4 +3,12 @@ import battlecode.common.*;
 
 public class Comms {
 
+	static int getNumGardeners(RobotController rc) throws GameActionException {
+		return rc.readBroadcast(0);
+	}
+	
+	static void writeNumGardeners(RobotController rc, int num) throws GameActionException {
+		rc.broadcast(0, num);
+	}
+	
 }
