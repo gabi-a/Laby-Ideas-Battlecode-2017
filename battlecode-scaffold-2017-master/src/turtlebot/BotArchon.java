1b53e59b5ee51a1986c4c28b1b9f61fda3bf5a58
gabi-a <abrahams.gabi@gmail.com>
2017-01-11 08:10:39 +1100

    Archons group together at the start

--- battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
@@ -7,14 +7,49 @@ import java.util.logging.Logger;
 public class BotArchon {
 	static RobotController rc;
 	
+	static MapLocation homeLocation;
+	
 	public static void turn(RobotController rc) throws GameActionException {
 		BotArchon.rc = rc;
 		
+		if(rc.getRoundNum() == 1) {
+			Comms.writeArchonLocation(rc);
+			return;
+		}
+		
+		if(rc.getRoundNum() == 2) {
+			MapLocation[] archonLocations = Comms.readArchonLocations(rc);
+			int dx = 0;
+			int dy = 0;
+			int archonCount = 0;
+			for(int i = 3; i-- > 1;) {
+				if(archonLocations[i] != null) {
+					archonCount++;
+					dx += archonLocations[i].x;
+					dy += archonLocations[i].y;
+				}
+			}
+			homeLocation = new MapLocation(dx/archonCount, dy/archonCount);
+			return;
+		}
+		
 		int gardenersCount = Comms.getNumGardeners(rc);
 		
-		if(gardenersCount < 2) {
-			
+		if(gardenersCount <= 2) {
+			if(rc.isBuildReady() && rc.getTeamBullets() >= RobotType.GARDENER.bulletCost) {
+				for(int direction = 360; (direction -= 15) > 0;) {
+					Direction directionToBuild = new Direction((float)Math.toRadians((double)direction));
+					if(rc.canBuildRobot(RobotType.GARDENER, directionToBuild)) {
+						rc.buildRobot(RobotType.GARDENER, directionToBuild);
+						gardenersCount++;
+						Comms.writeNumGardeners(rc, gardenersCount);
+						break;
+					}
+				}
+			}
 		}
+		
+		Nav.tryMove(rc, rc.getLocation().directionTo(homeLocation));
 		
 	}
 }
--- battlecode-scaffold-2017-master/src/turtlebot/Comms.java
+++ battlecode-scaffold-2017-master/src/turtlebot/Comms.java
@@ -3,12 +3,41 @@ import battlecode.common.*;
 
 public class Comms {
 
-	static int getNumGardeners(RobotController rc) throws GameActionException {
+	// Uses channel 0
+	public static int getNumGardeners(RobotController rc) throws GameActionException {
 		return rc.readBroadcast(0);
 	}
 	
-	static void writeNumGardeners(RobotController rc, int num) throws GameActionException {
+	// Uses channel 0
+	public static void writeNumGardeners(RobotController rc, int num) throws GameActionException {
 		rc.broadcast(0, num);
+	}
+
+	// Uses channels 1 to 6
+	// There could be an issue if one coordinate is 0
+	public static void writeArchonLocation(RobotController rc) throws GameActionException {
+		for(int channel = 8; (channel-=2) > 1;) {
+			int x = rc.readBroadcast(channel);
+			int y = rc.readBroadcast(channel+1);
+			if(x == 0 && y == 0) {
+				rc.broadcast(channel, (int) rc.getLocation().x);
+				rc.broadcast(channel+1, (int) rc.getLocation().y);
+				System.out.println(channel);
+				break;
+			}
+		}
+	}
+	
+	public static MapLocation[] readArchonLocations(RobotController rc) throws GameActionException {
+		MapLocation[] archonLocations = new MapLocation[3];
+		for(int channel = 8; (channel-=2) > 1;) {
+			int x = rc.readBroadcast(channel);
+			int y = rc.readBroadcast(channel+1);
+			if(x != 0 && y != 0) {
+				archonLocations[channel/2 - 1] = new MapLocation(x,y);
+			}
+		}
+		return archonLocations;
 	}
 	
 }
