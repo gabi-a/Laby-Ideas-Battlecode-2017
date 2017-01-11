302caa80750d320f250527bbd1514147e705534a
gabi-a <abrahams.gabi@gmail.com>
2017-01-12 08:37:43 +1100

    Moved bullet avoidance to Nav

--- battlecode-scaffold-2017-master/src/turtlebot_gabisgardeners/Nav.java
+++ battlecode-scaffold-2017-master/src/turtlebot_gabisgardeners/Nav.java
@@ -112,4 +112,50 @@ public class Nav {
     		moveAttemptCount++;
     	}
     }
+    
+    public static boolean avoidBullets(RobotController rc, MapLocation myLocation) throws GameActionException {
+		
+		BulletInfo[] bullets = rc.senseNearbyBullets();
+		BulletInfo bullet;
+		MapLocation goalLoc = myLocation;
+		if(bullets.length == 0) {
+			return false;
+		}
+		for(int i = bullets.length;i-->0;) {
+			bullet = bullets[i];
+			if(willCollideWithMe(rc, bullet, myLocation)) {
+				goalLoc = goalLoc.add(bullet.dir.rotateLeftDegrees(90));
+			}
+		}
+		if(goalLoc == myLocation) {
+			return false;
+		}
+		return Nav.tryMove(rc, myLocation.directionTo(goalLoc));
+		
+	}
+	
+	static boolean willCollideWithMe(RobotController rc, BulletInfo bullet, MapLocation loc) {
+
+        // Get relevant bullet information
+        Direction propagationDirection = bullet.dir;
+        MapLocation bulletLocation = bullet.location;
+
+        // Calculate bullet relations to this robot
+        Direction directionToRobot = bulletLocation.directionTo(loc);
+        float distToRobot = bulletLocation.distanceTo(loc);
+        float theta = propagationDirection.radiansBetween(directionToRobot);
+
+        // If theta > 90 degrees, then the bullet is traveling away from us and we can break early
+        if (Math.abs(theta) > Math.PI/2) {
+            return false;
+        }
+
+        // distToRobot is our hypotenuse, theta is our angle, and we want to know this length of the opposite leg.
+        // This is the distance of a line that goes from myLocation and intersects perpendicularly with propagationDirection.
+        // This corresponds to the smallest radius circle centered at our location that would intersect with the
+        // line that is the path of the bullet.
+        float perpendicularDist = (float)Math.abs(distToRobot * Math.sin(theta)); // soh cah toa :)
+
+        return (perpendicularDist <= rc.getType().bodyRadius);
+    }
 }
