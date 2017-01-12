074bb65b19419fc07f9b96fe0e880eb759d0b51f
gabi-a <abrahams.gabi@gmail.com>
2017-01-12 14:27:43 +1100

    Fixed formatting

--- battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
@@ -3,76 +3,48 @@ package turtlebot;
 import battlecode.common.*;
 
 public class BotArchon {
-    
-        public static final float GARDEN_DISTANCE = 10f;
-        public static final int EXPLORE_SPOKES = 24;
-    
+
+	public static final float GARDEN_DISTANCE = 10f;
+	public static final int EXPLORE_SPOKES = 24;
+
 	public static int count = 0;
-        public static boolean startupFlag = true;
+	public static boolean startupFlag = true;
 	public static Direction[] exploreDirections = new Direction[EXPLORE_SPOKES];
 	public static int exploreDirectionsPointer = 0;
-	
+
 	public static void turn(RobotController rc) throws GameActionException {
-		
-		if(rc.getTeamBullets() >= 10000) {
+
+		if (!Nav.explore(rc)) {
+			System.out.format("\nCouldn't move");
+		}
+
+		if (rc.getTeamBullets() >= 10000) {
 			rc.donate(10000);
 		}
 
 		MapLocation selfLoc = rc.getLocation();
-                
-                if (!Nav.explore(rc)) {
-                    System.out.format("\nCouldn't move");
-                }
-		
-		if(startupFlag) {
-			Comms.writeGarden(rc, selfLoc);
-                        Direction exploreDir = new Direction(0);
-                        for (int i=0; i < EXPLORE_SPOKES; i++) {
-                            exploreDirections[i] = exploreDir;
-                            exploreDir = exploreDir.rotateLeftRads(7 * (float) Math.PI / EXPLORE_SPOKES);
-                        }
-                        startupFlag = false;
-		}
-		
-		if( !rc.hasMoved()) {
-                    Nav.avoidBullets(rc, selfLoc);
-                }
 
-		if (count <= 7) {
-			count += tryHireGardener(rc) ? 1 : 0;
-		}              
-		
-		
-                // Magic line
-                while(checkUnassignedScout(rc)) {}
+		if (rc.getRoundNum() == 1) {
+			Comms.writeGarden(rc, selfLoc);
+		}
+
+		// Nav.avoidBullets(rc, selfLoc);
+
+		while (count <= 7) {
+			count += BotArchon.tryHireGardener(rc) ? 1 : 0;
+		}
+
 	}
-    
-    public static boolean tryHireGardener(RobotController rc) throws GameActionException {
-        Direction hireDirection = new Direction(0);
-        for (int i =0 ; i < 8; i++) {
-            if(rc.canHireGardener(hireDirection) && rc.onTheMap(rc.getLocation().add(hireDirection, 5f))) {
-                rc.hireGardener(hireDirection);
-                return true;
-            }
-            hireDirection = hireDirection.rotateLeftRads((float) Math.PI * 0.25f);
-        }
-        return false;
-    }
-    
-    public static void delegateScout(RobotController rc) throws GameActionException {
-        MapLocation exploreLocation = rc.getInitialArchonLocations(rc.getTeam())[0].add(exploreDirections[exploreDirectionsPointer], 100f);
-        Comms.writeStack(rc, Comms.ARCHON_SCOUT_DELEGATION_START, Comms.ARCHON_SCOUT_DELEGATION_END, exploreLocation);
-        exploreDirectionsPointer++;
-        exploreDirectionsPointer %= 24;
-    }
-    
-    public static boolean checkUnassignedScout(RobotController rc) throws GameActionException {
-        MapLocation unassignedCheck = Comms.popStack(rc, Comms.SCOUT_ARCHON_REQUEST_START, Comms.SCOUT_ARCHON_REQUEST_END);
-        if(unassignedCheck != null) {
-            delegateScout(rc);
-            System.out.println("DELEGATED");
-            return true;
-        }
-        return false;
-    }
+
+	public static boolean tryHireGardener(RobotController rc) throws GameActionException {
+		Direction hireDirection = new Direction(0);
+		for (int i = 0; i < 8; i++) {
+			if (rc.canHireGardener(hireDirection) && rc.onTheMap(rc.getLocation().add(hireDirection, 5f))) {
+				rc.hireGardener(hireDirection);
+				return true;
+			}
+			hireDirection = hireDirection.rotateLeftRads((float) Math.PI * 0.25f);
+		}
+		return false;
+	}
 }
--- battlecode-scaffold-2017-master/src/turtlebot/BotScout.java
+++ battlecode-scaffold-2017-master/src/turtlebot/BotScout.java
@@ -3,94 +3,90 @@ package turtlebot;
 import battlecode.common.*;
 
 public class BotScout {
-    
-        public static MapLocation moveTarget = null;
-        public static boolean startupFlag = true;
-        public static MapLocation homeMemoryLocation = null;
-        public static boolean returning = false;
-        public static RobotInfo enemyTarget = null;
-        public static int trappedCount = 0;
 
-        public static void turn(RobotController rc) throws GameActionException {
+	public static MapLocation moveTarget = null;
+	public static boolean startupFlag = true;
+	public static MapLocation homeMemoryLocation = null;
+	public static boolean returning = false;
+	public static RobotInfo enemyTarget = null;
+	public static int trappedCount = 0;
+
+	public static void turn(RobotController rc) throws GameActionException {
 		MapLocation myLocation = rc.getLocation();
-                
-                RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
-                enemyTarget = null;
 
-		for(int i = 0; i < enemies.length; i++){
-			if(enemies[i].getType() == RobotType.GARDENER || rc.getRoundNum() > 500){
-				//Comms.writeStack(rc, Comms.ENEMY_ARCHON_START, Comms.ENEMY_ARCHON_END, enemies[i].getLocation());
+		RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+		enemyTarget = null;
+
+		for (int i = 0; i < enemies.length; i++) {
+			if (enemies[i].getType() == RobotType.GARDENER || rc.getRoundNum() > 500) {
+				// Comms.writeStack(rc, Comms.ENEMY_ARCHON_START,
+				// Comms.ENEMY_ARCHON_END, enemies[i].getLocation());
 				enemyTarget = enemies[i];
 			} else {
-				//Comms.writeStack(rc, Comms.ENEMY_START, Comms.ENEMY_END, enemies[i].getLocation());
+				// Comms.writeStack(rc, Comms.ENEMY_START, Comms.ENEMY_END,
+				// enemies[i].getLocation());
 			}
 		}
-                
-                if(startupFlag) {
-                   System.out.format("I'm here!\n");
-                   homeMemoryLocation = myLocation;
-                   System.out.format("My home location: (%f, %f)\n", myLocation.x, myLocation.y);
-                   startupFlag = false;
-                }
-                
-                if( moveTarget == null) {
-                    moveTarget = Comms.popStack(rc, Comms.ARCHON_SCOUT_DELEGATION_START, Comms.ARCHON_SCOUT_DELEGATION_END);
-                    if (moveTarget == null) {
-                        //System.out.format("Failed to get comms, while %b returning\n", returning);
-                    }
-                    else {
-                        System.out.format("My assignment: (%f, %f)\n", moveTarget.x, moveTarget.y);
-                    }
-                }
-                
-		if(enemyTarget == null) {
-                        if (moveTarget == null && returning == false) {
-                            //Nav.explore(rc);
-                        }
-                        else {
-                            if (!returning) {
-                                Direction moveDirection = new Direction(myLocation, moveTarget); 
-                                boolean successful = Nav.tryMove(rc, moveDirection);
-                                trappedCount += successful ? 0 : 1;
-                                if(
-                                        (!rc.onTheMap(myLocation.add(moveDirection, 5f))) 
-                                        || trappedCount > 15
-                                   ) {
-                                    returning = true;
-                                    trappedCount = 0;
-                                    System.out.format("I'm returning from: (%f, %f)\n", myLocation.x, myLocation.y);
-                                }
-                            }
-                            else {
-                                Direction moveDirection = new Direction(myLocation, homeMemoryLocation); 
-                                boolean hasMoved = Nav.tryMove(rc, moveDirection);
-                                trappedCount += hasMoved ? 0 : 1;
-                                if(myLocation.distanceTo(homeMemoryLocation) < 3f || trappedCount > 15) {
-                                    returning = false;
-                                    moveTarget = null;
-                                    trappedCount = 0;
-                                    broadcastUnassigned(rc);
-                                    System.out.format("I'm unassigned now...\n");
-                                }
-                            }
-                        }
+
+		if (startupFlag) {
+			System.out.format("I'm here!\n");
+			homeMemoryLocation = myLocation;
+			System.out.format("My home location: (%f, %f)\n", myLocation.x, myLocation.y);
+			startupFlag = false;
 		}
-                else {
+
+		if (moveTarget == null) {
+			moveTarget = Comms.popStack(rc, Comms.ARCHON_SCOUT_DELEGATION_START, Comms.ARCHON_SCOUT_DELEGATION_END);
+			if (moveTarget == null) {
+				// System.out.format("Failed to get comms, while %b
+				// returning\n", returning);
+			} else {
+				System.out.format("My assignment: (%f, %f)\n", moveTarget.x, moveTarget.y);
+			}
+		}
+
+		if (enemyTarget == null) {
+			if (moveTarget == null && returning == false) {
+				// Nav.explore(rc);
+			} else {
+				if (!returning) {
+					Direction moveDirection = new Direction(myLocation, moveTarget);
+					boolean successful = Nav.tryMove(rc, moveDirection);
+					trappedCount += successful ? 0 : 1;
+					if ((!rc.onTheMap(myLocation.add(moveDirection, 5f))) || trappedCount > 15) {
+						returning = true;
+						trappedCount = 0;
+						System.out.format("I'm returning from: (%f, %f)\n", myLocation.x, myLocation.y);
+					}
+				} else {
+					Direction moveDirection = new Direction(myLocation, homeMemoryLocation);
+					boolean hasMoved = Nav.tryMove(rc, moveDirection);
+					trappedCount += hasMoved ? 0 : 1;
+					if (myLocation.distanceTo(homeMemoryLocation) < 3f || trappedCount > 15) {
+						returning = false;
+						moveTarget = null;
+						trappedCount = 0;
+						broadcastUnassigned(rc);
+						System.out.format("I'm unassigned now...\n");
+					}
+				}
+			}
+		} else {
 			Direction dir = rc.getLocation().directionTo(enemyTarget.location);
 			float dist = enemyTarget.location.distanceTo(rc.getLocation());
-			if(!Nav.avoidBullets(rc, myLocation)) {
-                            if (dist >= 0.5f) {
-                                Nav.tryMove(rc, dir);
-                            }
+			if (!Nav.avoidBullets(rc, myLocation)) {
+				if (dist >= 0.5f) {
+					Nav.tryMove(rc, dir);
+				}
 			}
-                        if(rc.canFireSingleShot()) {
-                            rc.fireSingleShot(dir);
-                        }
+			if (rc.canFireSingleShot()) {
+				rc.fireSingleShot(dir);
+			}
 		}
-        }
+	}
 
-        public static void broadcastUnassigned(RobotController rc) throws GameActionException {
-            Comms.writeStack(rc, Comms.SCOUT_ARCHON_REQUEST_START, Comms.SCOUT_ARCHON_REQUEST_END, rc.getLocation());
-        }
+	public static void broadcastUnassigned(RobotController rc) throws GameActionException {
+		Comms.writeStack(rc, Comms.SCOUT_ARCHON_REQUEST_START, Comms.SCOUT_ARCHON_REQUEST_END, rc.getLocation());
+	}
 
 }
