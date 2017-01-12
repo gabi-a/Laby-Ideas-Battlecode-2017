70d24b10850caa05b095882696905d7a6c9b3fda
tryth <tryth@tryth>
2017-01-12 18:45:55 +1100

    Refractor archon

--- battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/turtlebot/BotArchon.java
@@ -9,8 +9,8 @@ public class BotArchon {
 
 	public static int count = 0;
 	public static boolean startupFlag = true;
-	public static Direction[] exploreDirections = new Direction[EXPLORE_SPOKES];
-	public static int exploreDirectionsPointer = 0;
+	public static MapLocation[] exploreLocations = new MapLocation[EXPLORE_SPOKES];
+	public static int exploreLocationsPointer = 0;
 
 	public static void turn(RobotController rc) throws GameActionException {
 
@@ -29,7 +29,7 @@ public class BotArchon {
 			Comms.writeGarden(rc, selfLoc);
 			Direction exploreDir = new Direction(0);
 			for (int i=0; i < EXPLORE_SPOKES; i++) {
-				exploreDirections[i] = exploreDir;
+				exploreLocations[i] = selfLoc.add(exploreDir,100f);
 				exploreDir = exploreDir.rotateLeftRads(7 * (float) Math.PI / EXPLORE_SPOKES);
 			}
 		}
@@ -58,10 +58,9 @@ public class BotArchon {
 	}
 	
 	public static void delegateScout(RobotController rc) throws GameActionException {
-        MapLocation exploreLocation = rc.getInitialArchonLocations(rc.getTeam())[0].add(exploreDirections[exploreDirectionsPointer], 100f);
-        Comms.writeStack(rc, Comms.ARCHON_SCOUT_DELEGATION_START, Comms.ARCHON_SCOUT_DELEGATION_END, exploreLocation);
-        exploreDirectionsPointer++;
-        exploreDirectionsPointer %= EXPLORE_SPOKES;
+        Comms.writeStack(rc, Comms.ARCHON_SCOUT_DELEGATION_START, Comms.ARCHON_SCOUT_DELEGATION_END, exploreLocations[exploreLocationsPointer]);
+        exploreLocationsPointer++;
+        exploreLocationsPointer %= exploreLocations.length;
     }
     
     public static boolean checkUnassignedScout(RobotController rc) throws GameActionException {
