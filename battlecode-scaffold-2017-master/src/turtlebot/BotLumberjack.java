cadcc7619f3cff3b9190cfb90bb8cc07552b0dd5
Ben Harper <benharper888@gmail.com>
2017-01-12 16:11:49 +1100

    it's ugly as balls but it'll do for now. Sorry Gabi, it's more unreadable than before

--- battlecode-scaffold-2017-master/src/turtlebot/BotLumberjack.java
+++ battlecode-scaffold-2017-master/src/turtlebot/BotLumberjack.java
@@ -5,7 +5,9 @@ import battlecode.common.*;
 public class BotLumberjack {
 	static RobotController rc;
 	static TreeInfo target = null;
+	static TreeInfo path_target = null;
 	static final int HOME_RADIUS= 20;
+	static int moves_blocked = 0;
 	
 	static boolean toldArchonsImDead = false;
 	
@@ -32,24 +34,8 @@ public class BotLumberjack {
 				if(target != null){
 
 				} else {
-					TreeInfo[] trees = new TreeInfo[0];
-					trees = rc.senseNearbyTrees();
-					if(trees.length == 0){
-						Direction dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
-						while(!rc.canMove(dir))
-							dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
-						rc.move(dir);
-						return;
-					}
-					int tree_id = -1;
-					for(int i = 0; i < trees.length; i++){
-						if(trees[i].getTeam() == Team.NEUTRAL && (trees[i].getContainedBullets() != 0 || trees[i].getContainedRobot() != null)){
-							tree_id = i;
-							break;
-						}
-					}
-
-					if(tree_id == -1){
+					target = nearestTree(rc);
+					if(target == null){
 						MapLocation loc = new MapLocation(0,0);
 						Direction dir = rc.getLocation().directionTo(loc).opposite();
 						while(!rc.canMove(dir))
@@ -57,18 +43,16 @@ public class BotLumberjack {
 						rc.move(dir);
 						return;
 					}
-
-					target = trees[tree_id];
-					for(int i = 0; i < trees.length; i++){
-						if(trees[i].getTeam() == Team.NEUTRAL && (trees[i].getContainedBullets() != 0 || trees[i].getContainedRobot() != null)){
-							Comms.pushLowPriorityTree(rc, trees[i], 3);
-							System.out.println("added tree to queue");
-						}
-					}
 				}
 			}
 		}
 
+		if(path_target != null){
+				if(rc.canChop(path_target.getID())) rc.chop(path_target.getID());
+
+				// Stop trying to chop if the tree is dead
+				if(rc.getLocation().distanceTo(path_target.getLocation()) < 7 && !rc.canSenseTree(path_target.getID())) path_target = null;
+		}
 		if(rc.canChop(target.getID())){
 			rc.chop(target.getID());
 			return;
@@ -76,15 +60,49 @@ public class BotLumberjack {
 		if(rc.canMove(target.getLocation())){
 			rc.move(target.getLocation());
 		} else {
-			MapLocation loc = new MapLocation(0,0);
-			Direction dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
-			while(!rc.canMove(dir))
-				dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
-			rc.move(dir);
+			moves_blocked++;
+			if(moves_blocked < 10){
+				MapLocation loc = new MapLocation(0,0);
+				Direction dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
+				while(!rc.canMove(dir))
+					dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
+				rc.move(dir);
+			} else {
+				path_target = nearestTree(rc);
+				moves_blocked = 0;
+			}
+			if(path_target != null){
+				if(rc.canChop(path_target.getID())) rc.chop(path_target.getID());
+
+				// Stop trying to chop if the tree is dead
+				if(rc.getLocation().distanceTo(path_target.getLocation()) < 7 && !rc.canSenseTree(path_target.getID())) path_target = null;
+			}
 		}
 		if(rc.canChop(target.getID())) rc.chop(target.getID());
 
 		// Stop trying to chop if the tree is dead
 		if(rc.getLocation().distanceTo(target.getLocation()) < 7 && !rc.canSenseTree(target.getID())) target = null;
+	}
+
+	public static TreeInfo nearestTree(RobotController rc) throws GameActionException {
+		TreeInfo[] trees = new TreeInfo[0];
+		trees = rc.senseNearbyTrees();
+		int tree_id = -1;
+		float dist = 200;
+		for(int i = 0; i < trees.length; i++){
+			float new_dist = rc.getLocation().distanceTo(trees[i].getLocation());
+			if(trees[i].getTeam() == Team.NEUTRAL && new_dist < dist){
+				tree_id = i;
+				dist = new_dist;
+			}
+		}
+		if(trees.length == 0 || tree_id == -1){
+			Direction dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
+			while(!rc.canMove(dir))
+				dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
+			rc.move(dir);
+			return null;
+		}
+		return trees[tree_id];
 	}
 }
