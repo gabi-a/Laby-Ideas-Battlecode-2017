32b7a894c256e242d1fd47a46939e17072666839
gabi-a <abrahams.gabi@gmail.com>
2017-01-16 10:13:15 +1100

    added hire method to archon

--- battlecode-scaffold-2017-master/src/SprintBot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/SprintBot/BotArchon.java
@@ -11,5 +11,17 @@ public class BotArchon {
 		BotArchon.rc = rc;
 	}
 	
+	public static boolean tryHireGardener() throws GameActionException {
+		Direction hireDirection = new Direction(0);
+		for (int i = 0; i < 8; i++) {
+			if (rc.canHireGardener(hireDirection) && rc.onTheMap(rc.getLocation().add(hireDirection, 5f))) {
+				rc.hireGardener(hireDirection);
+				return true;
+			}
+			hireDirection = hireDirection.rotateLeftRads((float) Math.PI * 0.25f);
+		}
+		return false;
+	}
+	
 }
 
