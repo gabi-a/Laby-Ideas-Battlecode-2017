e1fb71907f02037bbfde129683deb226de4d354f
Ben Harper <benharper888@gmail.com>
2017-01-16 13:23:21 +1100

    simple offensive lumberjacks

--- battlecode-scaffold-2017-master/src/SprintBot/BotLumberjack.java
+++ battlecode-scaffold-2017-master/src/SprintBot/BotLumberjack.java
@@ -9,20 +9,62 @@ enum Strategy {
 public class BotLumberjack {
 	static RobotController rc;
 
-	static TreeInfo target;
+	static TreeInfo treeTarget;
+	static RobotInfo enemyTarget;
 	static MapLocation home;
-	static Strategy strat = Strategy.LUMBERJACK;
+	static Strategy strat;
 	
 	public static void turn(RobotController rc) throws GameActionException {
 		BotLumberjack.rc = rc;
 
+		if(strat == null){
+			float randStrat = (float)Math.random();
+			if(randStrat < 0.0) strat = Strategy.LUMBERJACK;
+			else strat = Strategy.OFFENSE;
+		}
+
+		if(strat == Strategy.OFFENSE) {
+			// update target
+			if(enemyTarget != null){
+				if(!rc.canSenseRobot(enemyTarget.getID())) enemyTarget = null;
+				else enemyTarget = rc.senseRobot(enemyTarget.getID());
+			}
+
+			// Find target
+			if(enemyTarget == null){
+				RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+				if(enemies.length == 0){
+					Nav.explore(rc);
+					return;
+				}
+				float dist = 200;
+				for(int i = 0; i < enemies.length; i++){
+					float newDist = enemies[i].getLocation().distanceTo(rc.getLocation());
+					if(newDist < dist){
+						dist = newDist;
+						enemyTarget = enemies[i];
+					}
+				}
+			}
+
+			// get in range and kill
+			Nav.tryMove(rc, rc.getLocation().directionTo(enemyTarget.getLocation()));
+			float strikeRadius = 2 + enemyTarget.getType().bodyRadius;
+			if(rc.getLocation().distanceTo(enemyTarget.getLocation()) <= strikeRadius){
+				rc.strike();
+			}
+		}
+
+		if(strat == Strategy.DEFENSE) {
+
+		}
 
 		if(strat == Strategy.LUMBERJACK){
 			// Find a target
-			if(target == null) {
-				target = Comms.neutralTrees.pop(rc);
+			if(treeTarget == null) {
+				treeTarget = Comms.neutralTrees.pop(rc);
 			}
-			if(target == null) {
+			if(treeTarget == null) {
 				TreeInfo[] trees = rc.senseNearbyTrees();
 				if(trees.length == 0){
 					Nav.explore(rc);
@@ -33,24 +75,24 @@ public class BotLumberjack {
 					float newDist = trees[i].getLocation().distanceTo(rc.getLocation());
 					if(newDist < dist){
 						dist = newDist;
-						target = trees[i];
+						treeTarget = trees[i];
 					}
 				}
 			}
 
 			// Go to target
-			if(!rc.canInteractWithTree(target.getID())){
-				Nav.tryMove(rc, rc.getLocation().directionTo(target.getLocation()));
+			if(!rc.canInteractWithTree(treeTarget.getID())){
+				Nav.tryMove(rc, rc.getLocation().directionTo(treeTarget.getLocation()));
 			}
 
 			// Chop target
-			if(rc.canInteractWithTree(target.getID())){
-				rc.chop(target.getID());
+			if(rc.canInteractWithTree(treeTarget.getID())){
+				rc.chop(treeTarget.getID());
 			}
 
 			// Check if target is dead
-			if(rc.canSenseLocation(target.getLocation()) && !rc.canSenseTree(target.getID())) {
-				target = null;
+			if(rc.canSenseLocation(treeTarget.getLocation()) && !rc.canSenseTree(treeTarget.getID())) {
+				treeTarget = null;
 			}
 		}
 	}
