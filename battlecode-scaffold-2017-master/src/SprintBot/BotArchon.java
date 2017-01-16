2aa7b5df16f080ce1d1339d723553739b64f96fd
gabi-a <abrahams.gabi@gmail.com>
2017-01-16 16:11:48 +1100

    Replaced archon with SprintGardener archon

--- battlecode-scaffold-2017-master/src/SprintBot/BotArchon.java
+++ battlecode-scaffold-2017-master/src/SprintBot/BotArchon.java
@@ -4,24 +4,8 @@ import battlecode.common.*;
 public class BotArchon {
 	static RobotController rc;
 	
-<<<<<<< HEAD
-=======
-	static enum Archon {
-		FIRST,
-		SECOND,
-		THIRD
-	}
-	
-	static Archon archonDesignation;
-	static int numArchonsAtStart;
-	
-	static Team myTeam;
-	static MapLocation myLocation;
-	
->>>>>>> refs/heads/SprintScout
 	public static void turn(RobotController rc) throws GameActionException {
 		BotArchon.rc = rc;
-<<<<<<< HEAD
 		Util.updateBotCount(rc);
 		Util.reportDeath(rc);
 		
@@ -48,39 +32,6 @@ public class BotArchon {
 			hireDirection = hireDirection.rotateLeftRads((float) Math.PI * 0.25f);
 		}
 		return false;
-=======
-		
-		// CACHE COSTLY VARIABLE CALLS
-		myLocation = rc.getLocation();
-		myTeam = rc.getTeam();
-		
-		// SETUP
-		if(rc.getRoundNum() == 1) {
-			myTeam = rc.getTeam();
-			MapLocation[] archonLocs = rc.getInitialArchonLocations(myTeam); 
-			numArchonsAtStart = archonLocs.length;
-			for(int i = 0; i < archonLocs.length; i++) {
-				if(archonLocs[i] == myLocation) {
-					switch (i) {
-						case 0:
-							archonDesignation = Archon.FIRST;
-							break;
-						case 1:
-							archonDesignation = Archon.SECOND;
-							break;
-						case 2:
-							archonDesignation = Archon.THIRD;
-							break;
-						default:
-							System.out.println("Shouldn't get here!");
-					}
-				}
-			}
-			if(archonDesignation == Archon.FIRST) {
-				rc.hireGardener(Direction.getEast());
-			}
-		}
->>>>>>> refs/heads/SprintScout
 	}
 	
 }
